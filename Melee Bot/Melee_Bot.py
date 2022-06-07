bc56875b0324bf95425d60bc5825369567c86fac
thaggy <55408937+thaggy@users.noreply.github.com>
2022-06-07 00:21:10 -0500

    Added Gecko Codes for Libmelee to Function. Changed some logic so that it works for no matter the character being played by human

--- Melee Bot/Melee_Bot.py
+++ Melee Bot/Melee_Bot.py
@@ -24,21 +24,19 @@ while True:
         #We are in game here
         #print(gamestate.players[ai_port].action) # Prints the animation that player 2 is in
         #Detects incoming attacks
-        tech.fox_multishine(ai_state = gamestate.players[ai_port],controller = controller)
-        '''
+        print(framedata.is_attack(gamestate.players[human_port].character , gamestate.players[human_port].action))
         if gamestate.players[ai_port].action == melee.enums.Action.MARTH_COUNTER or gamestate.players[ai_port].action == melee.enums.Action.MARTH_COUNTER_FALLING or gamestate.players[ai_port].action == melee.enums.Action.PARASOL_FALLING:
             print("detecting counter state, entering tech.marth_counter")
             tech.marth_counter(ai_state = gamestate.players[ai_port], controller = controller)
-        if framedata.is_attack(22 , gamestate.players[human_port].action):
+        if framedata.is_attack(melee.enums.Character.FOX  , gamestate.players[human_port].action):
             hitframe = framedata.in_range(gamestate.players[human_port], gamestate.players[ai_port], melee.enums.Stage.POKEMON_STADIUM)
             framesuntilhit = hitframe - gamestate.players[human_port].action_frame
             print(framesuntilhit, " - ", hitframe, " - ", gamestate.players[human_port].action_frame)
             if framesuntilhit <=  6:
                 print("Detecting hit, starting counter")
                 tech.marth_counter(ai_state = gamestate.players[ai_port],controller = controller)
-        '''
            
         #    print(hitframe)
     else:
-       #Here we are in the main menu or menu select
-       melee.menuhelper.MenuHelper.menu_helper_simple(gamestate,controller,melee.enums.Character.FOX,melee.enums.Stage.FINAL_DESTINATION,connect_code="",autostart=False,swag=False)
+       #Here we are in the main menu or menu select]
+       melee.MenuHelper.menu_helper_simple(gamestate,controller,melee.enums.Character.MARTH,melee.enums.Stage.FINAL_DESTINATION,"",0,0,False,False)
